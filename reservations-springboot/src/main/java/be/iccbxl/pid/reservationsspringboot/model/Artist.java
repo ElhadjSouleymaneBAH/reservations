9ce6a3139f9909ff0b8b0983f2737757956c1a6c
ElhadjSouleymaneBAH <soulbl@yahoo.fr>
2024-11-03 13:20:13 +0100

    Controller artist +templates +routes

--- reservations-springboot/src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
+++ reservations-springboot/src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
@@ -1,21 +1,36 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
-
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "artists")
 @Getter
 @Setter
+@AllArgsConstructor
 @Builder
-@Table(name = "artists")
 public class Artist {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "The firstname must not be empty.")
+    @Size(min = 2, max = 60, message = "The firstname must be between 2 and 60 characters long.")
     private String firstname;
+
+    @NotBlank(message = "The lastname must not be empty.")
+    @Size(min = 2, max = 60, message = "The lastname must be between 2 and 60 characters long.")
     private String lastname;
+
+    protected Artist() {}
 }
 
