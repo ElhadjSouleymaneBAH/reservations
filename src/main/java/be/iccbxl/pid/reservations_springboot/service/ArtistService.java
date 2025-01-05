10394cf6ae38b9cf43139f210dbcb70070e2e114
ElhadjSouleymaneBAH <soulbl@yahoo.fr>
2025-01-05 22:40:59 +0100

    Controller Artist +templates +routes

--- src/main/java/be/iccbxl/pid/reservations_springboot/service/ArtistService.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/service/ArtistService.java
@@ -1,36 +1,41 @@
 package be.iccbxl.pid.reservations_springboot.service;
 
-import be.iccbxl.pid.reservations_springboot.model.Artist;
-import be.iccbxl.pid.reservations_springboot.repository.ArtistRepository;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import be.iccbxl.pid.reservations_springboot.model.Artist;
+import be.iccbxl.pid.reservations_springboot.repository.ArtistRepository;
 
 @Service
 public class ArtistService {
-
-    private final ArtistRepository artistRepository;
-
     @Autowired
-    public ArtistService(ArtistRepository artistRepository) {
-        this.artistRepository = artistRepository;
-    }
+    private ArtistRepository artistRepository;
 
     public List<Artist> getAllArtists() {
-        return artistRepository.findAll();
+        List<Artist> artists = new ArrayList<>();
+
+        artistRepository.findAll().forEach(artists::add);
+
+        return artists;
     }
 
-    public Optional<Artist> getArtistById(Long id) {
+    public Artist getArtist(long id) {
         return artistRepository.findById(id);
     }
 
-    public Artist saveArtist(Artist artist) {
-        return artistRepository.save(artist);
+    public void addArtist(Artist artist) {
+        artistRepository.save(artist);
     }
 
-    public void deleteArtist(Long id) {
+    public void updateArtist(long id, Artist artist) {
+        artistRepository.save(artist);
+    }
+
+    public void deleteArtist(long id) {
         artistRepository.deleteById(id);
     }
+
 }
