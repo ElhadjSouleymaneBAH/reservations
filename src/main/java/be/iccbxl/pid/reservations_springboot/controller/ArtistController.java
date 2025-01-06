7f0198d4d69e1ba4720f0988e9bc0ea13564f2d7
ElhadjSouleymaneBAH <soulbl@yahoo.fr>
2025-01-06 20:14:06 +0100

    Controller Artist+templates+routes

--- src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
@@ -1,21 +1,13 @@
 package be.iccbxl.pid.reservations_springboot.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 
 import be.iccbxl.pid.reservations_springboot.model.Artist;
 import be.iccbxl.pid.reservations_springboot.service.ArtistService;
@@ -25,18 +17,24 @@ public class ArtistController {
     @Autowired
     ArtistService service;
 
-    //…
+    @GetMapping("/artists")
+    public String index(Model model) {
+        List<Artist> artists = service.getAllArtists();
 
-    @DeleteMapping("/artists/{id}")
-    public String delete(@PathVariable long id, Model model) {
-        Artist existing = service.getArtist(id);
+        model.addAttribute("artists", artists);
+        model.addAttribute("title", "Liste des artistes");
 
-        if(existing!=null) {
-            service.deleteArtist(id);
-        }
+        return "artist/index";
+    }
 
-        return "redirect:/artists";
+    @GetMapping("/artists/{id}")
+    public String show(Model model, @PathVariable("id") long id) {
+        Artist artist = service.getArtist(id);
+
+        model.addAttribute("artist", artist);
+        model.addAttribute("title", "Fiche d'un artiste");
+
+        return "artist/show";
     }
 
 }
-
